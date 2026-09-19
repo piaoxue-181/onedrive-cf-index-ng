65b0f5abb1eab2829563e08e0c0471349e98ea5a
末雨乘风 <924592920@qq.com>
2026-09-19 08:32:14 +0800

    Update protectedRoutes and hiddenItems in site config
    Removed 'Personal Vault' from protectedRoutes and added it to hiddenItems.

--- config/site.config.js
+++ config/site.config.js
@@ -39,7 +39,8 @@ module.exports = {
 
   // [OPTIONAL] This is where you specify the folders that are password protected. It is an array of paths pointing to all
   // the directories in which you have .password set. Check the documentation for details.
-  protectedRoutes: ['/Personal Vault'],
+  protectedRoutes: [],
+  hiddenItems: ['Personal Vault', '个人保管库'],
 
   // [OPTIONAL] Use "" here if you want to remove this email address from the nav bar.
   email: 'mailto:admin@blowswind.cn',
